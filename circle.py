5c297c96676c4bdc0af4e4631d9ba539ecad5eba
Melih Teke <melihteke@gmail.com>
2020-06-08 00:11:44 +0300

    Circle.py commit

---
+++ circle.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+pi=3.14159
+r=input("Please specify 'r' value: ")
+cevre=2*pi*int(r)
+alan =pi*( int(r)*int(r))
+print("--"*10)
+print(r, "degerindeki")
+print("Cemberin cevresi: ", cevre)
+print("Cemberin alani  : ", alan)
+print("--"*10)
