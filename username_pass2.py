6b6c472410a8faa0dd36d20ca1d173f461f9a85a
Melih Teke <melihteke@gmail.com>
2020-06-08 00:43:53 +0300

    Commit for all

---
+++ username_pass2.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+username = input("Please enter your username :")
+password = input("Please enter your password :")
+
+if username == "abcdefg" and password == "12345678":
+	print("You are logged in to very critical Software")
+else:
+	print("Please enter correct username and password combination")
